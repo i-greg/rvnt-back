eba890245151f877a727f9f5715b51004a1176bb
jgreg <gregoriadisioannis@gmail.com>
2023-04-08 22:34:28 +0300

    changed routes, changed date n time type to arrays

--- api/models/event.js
+++ api/models/event.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const eventSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true },
-    time: { type: String, required: true },
+    time: { type: [String], required: true },
     price: { type: Number, required: true },
-    date: { type: String, required: true },
+    date: { type: [String], required: true },
     location: { type: String, required: true },
     image: { type: String, required: true },
     category_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Category' },
